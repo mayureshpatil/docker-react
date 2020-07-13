8f84202686ba51f601a95779f8b1ea90b01248a3
mayureshpatil <53110327+mayureshpatil@users.noreply.github.com>
2020-07-13 18:27:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello Mayuresh This is Lumiq Problem 2\n');
 });
 
 app.listen(PORT, HOST);
